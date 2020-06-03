d7be916534a1aa3791e01a10b89e7d6ead9028fb
acohen89 <54380035+acohen89@users.noreply.github.com>
2020-06-03 10:47:45 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+  }
+
+});
+server.listen(5000); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
