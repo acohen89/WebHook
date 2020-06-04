552e23234d34b5ff65163fdc125a36df16eb5657
acohen89 <54380035+acohen89@users.noreply.github.com>
2020-06-03 22:42:24 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const http = require('http');
+const mysql = require('mysql');
+
 var server = http.createServer(function (req, res) {   // 2 - creating server
+
+  if (req.url == '/') { //check the URL of the current request
+
+      // set response header
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+
+      // set response content
+      res.write('<html><body><p>This is home Page.</p></body></html>');
+      res.end();
 
   }
 
 });
 server.listen(5000); //3 - listen for any incoming requests
-
 console.log('Node.js web server at port 5000 is running..')
+
+/*
+var con = mysql.createConnection({ // Establishes Connection To MySql Database
+host: "157.230.222.102",
+user: "dsypwqmujp",
+password: "n52XUjDsVT"
+});
+con.connect(function(err) {
+  if (err) throw err;
+}); */
