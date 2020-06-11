60b52d0731f255edd8563a802ec887c055db579a
acohen89 <54380035+acohen89@users.noreply.github.com>
2020-06-11 10:24:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
 
 });
 
-console.log('Node.js web server at port 80 is running..')
-server.listen(80, "0.0.0.0"); //3 - listen for any incoming requests
+console.log('Node.js web server at port 8901 is running..')
+server.listen(8901, "0.0.0.0"); //3 - listen for any incoming requests
 /*
 var con = mysql.createConnection({ // Establishes Connection To MySql Database
 host: "157.230.222.102",
