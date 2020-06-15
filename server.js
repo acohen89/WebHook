ab6a036d58b9ed9466f03582cb59d311eb7be9ce
acohen89 <54380035+acohen89@users.noreply.github.com>
2020-06-15 10:02:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,43 @@ const http = require('http');
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({"extended": false});
 
+
+/*
 app.get('/', function(req, res){
   res.send("this is the home page");
 });
 app.listen(8901);
 console.log("Listening on port 8901");
-/*
+app.post('/', urlEncodedParser, function(req, res){
+    console.log(req);
+    fs.writeFile('data.json', req.body , function (err) {
+                  if (err) throw err;
+
+});
+});*/
 var server = http.createServer(function (req, res) {   // 2 - creating server
 
-  //send to 98.179.61.174:8901/webhookdata
-  if (req.url == '/webhookdata') { //check the URL of the current request
-      // set response header
-      console.log(req);
+  if (req.url == '/') { //check the URL of the current request
+      // set response header;
       res.writeHead(200, { 'Content-Type': 'text/html' });
       console.log("Incoming Req")
-
+      console.log(req.body);
       //set response content
+      fs.writeFile('data.json', req.body,  function (err) {
+       if (err) throw err;
+
+     });
       res.write('<html><body><p>This is home Page.</p></body></html>');
       res.end();
+
 
   }
 
 });
 
 console.log('Node.js web server at port 8901 is running..')
-server.listen(8901); //3 - listen for any incoming requests*/
+server.listen(8901, "0.0.0.0"); //3 - listen for any incoming requests
